fba8c4d82382c15096d79a37d30e33f3cfa45149
Numan Turkeri <numanturkeri@teknasyon.com>
2017-10-11 00:34:50 +0300

    guncellemeler geldı

--- EducationFirst2/app/src/main/java/halit/education/com/educationfirst/MainActivity.java
+++ EducationFirst2/app/src/main/java/halit/education/com/educationfirst/MainActivity.java
@@ -3,11 +3,24 @@ package halit.education.com.educationfirst;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 //TODO buraya bak guncellendımı
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO Bunnları ben yapdım sımdı
+        ArrayList<String> benimListem=benimListemiGetir();
+    }
+
+
+
+
+
+    public ArrayList<String> benimListemiGetir(){
+        ArrayList<String> dataList=new ArrayList<>();
+        return dataList;
     }
 }
