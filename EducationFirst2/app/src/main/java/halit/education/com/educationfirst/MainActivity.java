8f6e010426acbadd4712c9b99148e374a36c705b
Halit <halit.egitim@gmail.com>
2017-10-12 00:43:06 -0400

    Adim 2 Custom Dialog yapildi, iki edit text ve kayit, kapama butonlari yapildi

--- EducationFirst2/app/src/main/java/halit/education/com/educationfirst/MainActivity.java
+++ EducationFirst2/app/src/main/java/halit/education/com/educationfirst/MainActivity.java
@@ -1,11 +1,16 @@
 package halit.education.com.educationfirst;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,6 +27,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button mShowDialog = (Button) findViewById(R.id.btnShowDialog);
+
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_login,null);
+                final EditText mEmail = (EditText) mView.findViewById(R.id.Firma);
+                final EditText mPassword = (EditText) mView.findViewById(R.id.Ciro);
+
+
+                mBuilder.setPositiveButton("Kaydet", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                mBuilder.setNegativeButton("Kapat", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+
+                    }
+                });
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                            if (!mPassword.getText().toString().isEmpty()) {
+                                Toast.makeText(MainActivity.this, R.string.success_login_msg, Toast.LENGTH_SHORT).show();
+
+                                dialog.dismiss();
+                            } else {
+                                Toast.makeText(MainActivity.this, R.string.error_login_msg, Toast.LENGTH_SHORT).show();
+                            }
+
+                    }
+                });
+
+
+            }
+        });
 
         textView = (TextView) findViewById(R.id.descTxt);
         listView = (ListView) findViewById(R.id.listView);
