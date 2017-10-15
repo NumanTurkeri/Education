1843016369200297e15ec8c1a1f8737711fbaf72
Numan Turkeri <numanturkeri@teknasyon.com>
2017-10-15 22:46:53 +0300

    detail aktivity eklendi.

--- EducationFirst2/app/src/main/java/halit/education/com/educationfirst/MainActivity.java
+++ EducationFirst2/app/src/main/java/halit/education/com/educationfirst/MainActivity.java
@@ -14,15 +14,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
->>>>>>> 2538cee42153d375a35155963a9f5122a2f28be1
 import halit.education.com.educationfirst.controller.adapter.CompanyAdapter;
 import halit.education.com.educationfirst.model.AdapterItem;
 
@@ -30,14 +21,6 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     ListView listView;
     CompanyAdapter adapter;
-<<<<<<< HEAD
-
-    //TODO benim yeni yaptigim is bu
-
-
-=======
-//TODO ben numan bende yenı ıs olarka bunu yapdım 
->>>>>>> 2538cee42153d375a35155963a9f5122a2f28be1
     ArrayList<AdapterItem> adapterItems=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openDialog();
+               openDialog ();
             }
         });
 
@@ -76,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<AdapterItem> dataList = new ArrayList<>();
         return dataList;
     }
-
     public void openDialog() {
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
         View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
